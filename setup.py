587f9f6acb99def417e0706cb3cebd01323d3cd8
LuYF-Lemon-love <3555028709@qq.com>
2025-03-31 11:38:10 +0800

    000

--- setup.py
+++ setup.py
@@ -1,17 +1,3 @@
-# Copyright 2024 Bytedance Ltd. and/or its affiliates
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
 # setup.py is the fallback installation script when pyproject.toml does not work
 from setuptools import setup, find_packages
 import os
@@ -48,7 +34,9 @@ GPU_REQUIRES = ['liger-kernel', 'flash-attn']
 MATH_REQUIRES = ['math-verify']  # Add math-verify as an optional dependency
 VLLM_REQUIRES = ['tensordict<=0.6.2', 'vllm<=0.8.2']
 SGLANG_REQUIRES = [
-  'tensordict<=0.6.2', 
+  'tensordict<=0.6.2',
+  'sglang[all]==0.4.4.post3',
+  'tensordict<=0.6.2',
   'sglang[all]==0.4.4.post4',
   'torch-memory-saver>=0.0.5'
 ]
