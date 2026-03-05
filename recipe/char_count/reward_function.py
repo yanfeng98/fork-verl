7ad4a588227f3f6858c139b9639ddc01e9fb3ee0
LuYF-Lemon-love <3555028709@qq.com>
2026-03-05 17:51:07 +0800

    style: 添加类型注解并清理格式

--- recipe/char_count/reward_function.py
+++ recipe/char_count/reward_function.py
@@ -1,30 +1,11 @@
-# Copyright 2024 Bytedance Ltd. and/or its affiliates
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
-"""
-Reward function
-"""
-
 from verl.utils.reward_score import math_reward
 
-
-def char_count_reward_function(data_source, solution_str, ground_truth, extra_info=None):
+def char_count_reward_function(data_source, solution_str: str, ground_truth: str, extra_info=None) -> int:
     try:
-        last_boxed_string = math_reward.last_boxed_only_string(solution_str)
+        last_boxed_string: str = math_reward.last_boxed_only_string(solution_str)
         if last_boxed_string is None:
             return 0
-        solution = math_reward.remove_boxed(last_boxed_string)
+        solution: str = math_reward.remove_boxed(last_boxed_string)
         if solution == ground_truth:
             return 1
         else:
